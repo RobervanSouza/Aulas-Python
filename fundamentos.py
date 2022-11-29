d4762d9be5df41f06a5ca76f51270cf7c882dd62
Robervan Souza <rob.robervan@hotmail.com>
2022-11-29 08:32:00 -0300

    primeiro commit

---
+++ fundamentos.py
@@ -0,0 +1,10 @@
+print("Ola mundo ", "hello world")
+print("Nível 0. Este código está rente a margem do vídeo.")
+
+var = 10
+if(var == 10):
+    print("Estamos no bloco 1, isto é, a 4 espaçamentos da margem.")
+    print("Essa é a segunda linha do bloco 'if'. ")
+
+print("Agora, voltamos para o bloco principal do programa.")
+((((((()))))))
